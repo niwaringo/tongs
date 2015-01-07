c8005c84d95d59addc0594455065d4eaa26fa82b
niwaringo:mac <niwaringo@gmail.com>
2015-01-07 16:36:54 +0900

    add constractor

--- src/cooker.model.js
+++ src/cooker.model.js
@@ -1,3 +1,12 @@
-var CookerModel = function() {};
+/**
+ *  @constructor
+ *
+ *  @param {string} name
+ *  @param {string} value
+ */
+var CookerModel = function(name, value) {
+  this.name = encodeURIComponent(name);
+  this.value = encodeURIComponent(value);
+};
 
 module.exports = CookerModel;
--- test/cooker.model.test.js
+++ test/cooker.model.test.js
@@ -1,9 +1,10 @@
 var CookerModel = require('cooker.model');
-var co = new CookerModel();
 var assert = require('assert');
 
 describe('model', function() {
-  it('test', function() {
-    assert.strictEqual(col.hoge, 'hoge');
+  it('name and value is encoded', function() {
+    var co = new CookerModel('ネーム', 'ヴァリュー');
+    assert.strictEqual(co.name, '%E3%83%8D%E3%83%BC%E3%83%A0');
+    assert.strictEqual(co.value, '%E3%83%B4%E3%82%A1%E3%83%AA%E3%83%A5%E3%83%BC');
   });
 });
