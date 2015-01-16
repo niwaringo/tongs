012ef4dc52457cfb4e256e04c7090665f8af1127
niwaringo:mac <niwaringo@gmail.com>
2015-01-16 23:39:26 +0900

    saucelabs and travis cofig

--- test/e2e/protractor.saucelabs.conf.js
+++ test/e2e/protractor.saucelabs.conf.js
@@ -4,5 +4,6 @@ exports.config = {
   },
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
+  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
   specs: ['spec.js']
 };
