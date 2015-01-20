ab556fdb83238c6336208d5fcb0013062f9c677b
niwaringo:mac <niwaringo@gmail.com>
2015-01-20 23:13:29 +0900

    Change browser settings

--- test/e2e/protractor.saucelabs.conf.js
+++ test/e2e/protractor.saucelabs.conf.js
@@ -4,20 +4,9 @@ exports.config = {
   // },
   //
   multiCapabilities: [{
-    'browserName': 'chrome',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Protractor suite tests',
-    'version': '39',
-    'selenium-version': '2.44.0',
-    'platform': 'OS X 10.9'
+    'browserName': 'chrome'
   }, {
-    'browserName': 'firefox',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'Protractor suite tests',
-    'version': '34',
-    'selenium-version': '2.44.0'
+    'browserName': 'firefox'
   }],
   baseUrl: 'http://niwaringo.github.io/tongs/browser',
   sauceUser: process.env.SAUCE_USERNAME,
