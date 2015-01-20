a035ead775d1872d33e0af36d7c263d92f045ad5
niwaringo:mac <niwaringo@gmail.com>
2015-01-20 23:03:04 +0900

    Change capabilitiy -> multiCapabilities

--- test/e2e/protractor.saucelabs.conf.js
+++ test/e2e/protractor.saucelabs.conf.js
@@ -1,7 +1,24 @@
 exports.config = {
-  capabilities: {
-    'browserName': 'chrome'
-  },
+  // capabilities: {
+  //   'browserName': 'chrome'
+  // },
+  //
+  multiCapabilities: [{
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Protractor suite tests',
+    'version': '39',
+    'selenium-version': '2.44.0',
+    'platform': 'OS X 10.9'
+  }, {
+    'browserName': 'firefox',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Protractor suite tests',
+    'version': '34',
+    'selenium-version': '2.44.0'
+  }],
   baseUrl: 'http://niwaringo.github.io/tongs/browser',
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
