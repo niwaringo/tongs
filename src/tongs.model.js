b1860900fe15b4ecd493d08793f4379726e23a03
niwaringo:mac <niwaringo@gmail.com>
2015-01-22 15:40:52 +0900

    Create util module

--- src/tongs.model.js
+++ src/tongs.model.js
@@ -1,77 +1,6 @@
 'use strict';
 
-var opt = {
-  /**
-   * @param {string || date} date
-   */
-  'expires': function(date) {
-    var date_str = "";
-
-    switch(Object.prototype.toString.call(date)) {
-      case "[object Date]":
-        date_str = date.toUTCString();
-      break;
-
-      case "[object String]":
-        date_str = date;
-      break;
-
-      default:
-        date_str = "";
-    }
-
-    return concatKeyVal('expires', date_str);
-  },
-
-  'path': function(path) {
-    return concatKeyVal('path', path);
-  },
-
-  'domain': function(domain) {
-    return concatKeyVal('domain', domain);
-  },
-
-  'secure': function(secure) {
-    return secure;
-  }
-};
-
-/**
- * copy node-util extend
- */
-function extend(origin, add) {
-  var keys = Object.keys(add);
-  var i = keys.length;
-  while (i--) {
-    origin[keys[i]] = add[keys[i]];
-  }
-  return origin;
-}
-
-/**
- * @param {string} key
- * @param {string} value
- */
-function concatKeyVal(key, value) {
-  return key + '=' + value;
-}
-
-/**
- * @param {object} obj
- * @return {object}
- */
-function constructOptionObject(obj) {
-  var _obj = {};
-
-  Object.keys(obj).forEach(function(key) {
-    if (!(key in opt)) return;
-    _obj[key] = opt[key](obj[key]);
-  });
-
-  return _obj;
-}
-
-/******************** main ********************/
+var util = require('tongs.util');
 
 /**
  *  @constructor
@@ -85,7 +14,7 @@ var TongsModel = function(name, value, option) {
   this.attrs = [];
   this.option = option || {};
 
-  this.attrs.push(concatKeyVal(this._name, this._value));
+  this.attrs.push(util.concatKeyVal(this._name, this._value));
   this.updateOptions(this.option);
 };
 
@@ -103,7 +32,7 @@ TongsModel.prototype.value = function(new_value) {
   if (!new_value) return decodeURIComponent(this._value);
 
   this._value = new_value;
-  this.attrs[0] = concatKeyVal(this._name, this._value);
+  this.attrs[0] = util.concatKeyVal(this._name, this._value);
 
   return this._value;
 };
@@ -113,7 +42,7 @@ TongsModel.prototype.value = function(new_value) {
  * @return {this}
  */
 TongsModel.prototype.updateOptions = function(option) {
-  this.option = extend(this.option, constructOptionObject(option));
+  this.option = util.extend(this.option, util.constructOptionObject(option));
   this.attrs.length = 1;
   
   var options = Object.keys(this.option).map(function(key) {
@@ -147,7 +76,7 @@ TongsModel.prototype.save = function() {
  */
 TongsModel.prototype.remove = function(option) {
   option = option || {};
-  this.updateOptions(extend(option, {expires: new Date(1970, 1, 1)}));
+  this.updateOptions(util.extend(option, {expires: new Date(1970, 1, 1)}));
   this.save();
 };
 
