984670325ce2a897018aa984cd9c80e8e38a7867
niwaringo:mac <niwaringo@gmail.com>
2015-03-27 15:30:46 +0900

    Change ESLint rules

--- src/tongs.js
+++ src/tongs.js
@@ -1,8 +1,44 @@
-'use strict';
+"use strict";
 
-function Tongs(cookies) {}
+function reStructreCookie(cookieStr) {
+  var piece = cookieStr.split(/=/);
+  var name = piece.shift();
+  var value = piece.join("=");
+  return [name, value];
+}
 
-Tongs.prototype.cookie = function(name, value, option) {
+function expiresString(date) {
+  if (Object.prototype.toString.call(date) === "[object Date]") {
+    return date.toUTCString();
+  }
+  if (Object.prototype.toString.call(+date) === "[object Number]") {
+    return new Date(+new Date() + (date * 864e+5)).toUTCString();
+  }
+  return "";
+}
+
+
+function optionStringfy(option) {
+  if (!option) return "";
+  return ([
+    option.expires ? "; expires=" + expiresString(option.expires) : "",
+    option.path ? "; path=" + option.path : "",
+    option.domain ? "; domain=" + option.domain : "",
+    option.secure ? "; secure" : ""
+  ].join(""));
+}
+
+function cookieStringfy(name, value, option) {
+  var optionStr = optionStringfy(option);
+  return ([
+  name, "=", value,
+  optionStr
+  ].join(""));
+}
+
+function Tongs() {}
+
+Tongs.prototype.cookie = function cookie(name, value, option) {
   if (arguments.length > 0 && !value) return this.read(name);
 
   if (arguments.length === 0) return this.readAll();
@@ -15,11 +51,11 @@ Tongs.prototype.save = function(name, value, option) {
 };
 
 Tongs.prototype.each = function(callback) {
-  var cookie_stores = document.cookie.split('; ');
+  var cookieStores = document.cookie.split("; ");
   var cookies = [];
 
-  for (var i = 0, l = cookie_stores.length; i < l; i++) {
-    cookies = reStructreCookie(cookie_stores[i]);
+  for (var i = 0, l = cookieStores.length; i < l; i++) {
+    cookies = reStructreCookie(cookieStores[i]);
     callback.call(this, cookies);
   }
 };
@@ -49,62 +85,26 @@ Tongs.prototype.readAll = function() {
 Tongs.prototype.remove = function(name, option) {
   option = option || {};
   option.expires = -1;
-  document.cookie = cookieStringfy(name, '', option);
+  document.cookie = cookieStringfy(name, "", option);
   return !this.read(name);
 };
 
 Tongs.prototype.wideDomain = function() {
-  var parts = location.hostname.split('.').reverse();
-  var wide_domain = parts.shift();
-  var c = '__tongs_test__';
+  var parts = location.hostname.split(".").reverse();
+  var wideDomain = parts.shift();
+  var c = "__tongs_test__";
 
   for (var i = 0, l = parts.length; i < l; i++) {
-    wide_domain = parts[i] + '.' + wide_domain;
+    wideDomain = parts[i] + "." + wideDomain;
 
-    if (i === (l - 1)) return wide_domain;
+    if (i === (l - 1)) return wideDomain;
 
     this.save(c, c);
     if (this.read(c)) {
       this.remove(c);
-      return '.' + wide_domain;
+      return "." + wideDomain;
     }
   }
 };
 
 module.exports = Tongs;
-
-function reStructreCookie(cookie_str) {
-  var piece = cookie_str.split(/=/);
-  var name = piece.shift();
-  var value = piece.join('=');
-
-  return [name, value];
-}
-
-function cookieStringfy(name, value, option) {
-  var option_str = optionStringfy(option);
-  return ([
-  name, '=', value,
-  option_str
-  ].join(''));
-}
-
-function optionStringfy(option) {
-  if (!option) return '';
-  return ([
-    option.expires? '; expires=' + expiresString(option.expires): '',
-    option.path    ? '; path=' + option.path : '',
-    option.domain  ? '; domain=' + option.domain : '',
-    option.secure  ? '; secure' : ''
-  ].join(''));
-}
-
-function expiresString(date) {
-  if (Object.prototype.toString.call(date) === '[object Date]') {
-    return date.toUTCString();
-  }
-  if (Object.prototype.toString.call(+date) === '[object Number]') {
-    return new Date(+new Date() + (date * 864e+5)).toUTCString();
-  }
-  return '';
-}
