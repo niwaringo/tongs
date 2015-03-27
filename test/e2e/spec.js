24c7f1df41edf3c066db471156f5932ab5b5fee9
niwaringo:mac <niwaringo@gmail.com>
2015-03-27 16:09:10 +0900

    Change ESLint rules

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -1,13 +1,16 @@
+/*global browser, expect, tongs*/
+"use strict";
+
 // spec.js
 var optionExec = function(callback) {
-  browser.getCapabilities().then(function(s) { 
+  browser.getCapabilities().then(function(s) {
     if (s.caps_.browserName.match(/chrome|firefox/)) {
       callback();
     }
   });
 };
 
-describe('tongs top page', function() {
+describe("tongs top page", function() {
   browser.ignoreSynchronization = true;
 
   beforeEach(function() {
@@ -15,76 +18,76 @@ describe('tongs top page', function() {
     browser.manage().deleteAllCookies();
   });
 
-  it('read', function() {
+  it("read", function() {
     browser.executeScript(function() {
-      document.cookie = 'name=value';
+      document.cookie = "name=value";
     });
     var cookie = browser.executeScript(function() {
-      return tongs.cookie('name');
+      return tongs().cookie("name");
     });
-    expect(cookie).toEqual('value');
+    expect(cookie).toEqual("value");
   });
 
-  it('save', function() {
+  it("save", function() {
     browser.executeScript(function() {
-      tongs.cookie('name', 'value');
+      tongs().cookie("name", "value");
     });
-    browser.manage().getCookie('name').then(function(cookie) {
-      expect(cookie.name).toEqual('name');
-      expect(cookie.value).toEqual('value');
+    browser.manage().getCookie("name").then(function(cookie) {
+      expect(cookie.name).toEqual("name");
+      expect(cookie.value).toEqual("value");
     });
   });
 
-  it('remove', function() {
-    var cookie = browser.executeScript(function() {
-      document.cookie = 'name=value';
-      tongs.remove('name');
+  it("remove", function() {
+    browser.executeScript(function() {
+      document.cookie = "name=value";
+      tongs().remove("name");
     });
 
-    browser.manage().getCookie('name').then(function(cookie) {
+    browser.manage().getCookie("name").then(function(cookie) {
       expect(cookie).toBeNull();
     });
   });
 
-  it('read all available cookie', function() {
+  it("read all available cookie", function() {
     var cookies = browser.executeScript(function() {
-      tongs.cookie('name1', 'value1');
-      tongs.cookie('name2', 'value2');
-      return tongs.cookie();
+      tongs().cookie("name1", "value1");
+      tongs().cookie("name2", "value2");
+      return tongs().cookie();
     });
 
-    expect(cookies).toEqual([{name1: 'value1'}, {name2: 'value2'}]);
+    expect(cookies).toEqual([{name1: "value1"}, {name2: "value2"}]);
   });
 
-  it('wide domain', function() {
+  it("wide domain", function() {
     var cookies = browser.executeScript(function() {
-      return tongs.wideDomain();
+      return tongs().wideDomain();
     });
 
-    expect(cookies).toEqual('.dropboxusercontent.com');
+    expect(cookies).toEqual(".dropboxusercontent.com");
   });
 
 
   //chrome and firefox only
   optionExec(function() {
     // set cookie and get Date
-    it('expires(date)', function() {
+    it("expires(date)", function() {
       var date = browser.executeScript(function() {
         var tom = new Date();
         tom.setDate(tom.getDate() + 1);
-        tongs.cookie('name', 'value', {expires: tom});
+        tongs().cookie("name", "value", {expires: tom});
 
         return Math.floor(+tom / 1000);
       });
 
-      browser.manage().getCookie('name').then(function(cookie) {
+      browser.manage().getCookie("name").then(function(cookie) {
         expect(date).toEqual(Math.floor(cookie.expiry));
       });
     });
 
-    it('expires(number)', function() {
+    it("expires(number)", function() {
       browser.executeScript(function() {
-        tongs.cookie('name', 'value', {expires: 1});
+        tongs().cookie("name", "value", {expires: 1});
       });
 
       var tom = {};
@@ -93,7 +96,7 @@ describe('tongs top page', function() {
       tom.month = tom._date.getMonth();
       tom.date = tom._date.getDate();
 
-      browser.manage().getCookie('name').then(function(cookie) {
+      browser.manage().getCookie("name").then(function(cookie) {
         var _expiry = new Date(cookie.expiry * 1000);
         expect(_expiry.getFullYear()).toEqual(tom.year);
         expect(_expiry.getMonth()).toEqual(tom.month);
@@ -101,58 +104,58 @@ describe('tongs top page', function() {
       });
     });
 
-    it('path', function() {
+    it("path", function() {
       browser.executeScript(function() {
-        tongs.cookie('name', 'value', {path: '/'});
+        tongs().cookie("name", "value", {path: "/"});
       });
 
-      browser.manage().getCookie('name').then(function(cookie) {
-        expect(cookie.path).toEqual('/');
+      browser.manage().getCookie("name").then(function(cookie) {
+        expect(cookie.path).toEqual("/");
       });
     });
 
-    it('secure', function() {
+    it("secure", function() {
       browser.executeScript(function() {
-        tongs.cookie('name', 'value', {secure: true});
+        tongs().cookie("name", "value", {secure: true});
       });
 
-      browser.manage().getCookie('name').then(function(cookie) {
+      browser.manage().getCookie("name").then(function(cookie) {
         expect(cookie.secure).toEqual(true);
       });
     });
 
-    it('domain', function() {
+    it("domain", function() {
       browser.executeScript(function() {
-        tongs.cookie('name', 'value', {domain: '.dropboxusercontent.com'});
+        tongs().cookie("name", "value", {domain: ".dropboxusercontent.com"});
       });
 
-      browser.manage().getCookie('name').then(function(cookie) {
-        expect(cookie.domain).toEqual('.dropboxusercontent.com');
+      browser.manage().getCookie("name").then(function(cookie) {
+        expect(cookie.domain).toEqual(".dropboxusercontent.com");
       });
     });
 
-    it('remove with path error', function() {
+    it("remove with path error", function() {
       var result = browser.executeScript(function() {
-        tongs.cookie('name', 'value', {path: '/'});
-        return tongs.remove('name');
+        tongs().cookie("name", "value", {path: "/"});
+        return tongs().remove("name");
       });
 
       expect(result).toEqual(false);
 
-      browser.manage().getCookie('name').then(function(cookie) {
+      browser.manage().getCookie("name").then(function(cookie) {
         expect(cookie).not.toBeNull();
       });
     });
 
-    it('remove with path success', function() {
+    it("remove with path success", function() {
       var result = browser.executeScript(function() {
-        tongs.cookie('name', 'value', {path: '/'});
-        return tongs.remove('name', {path: '/'});
+        tongs().cookie("name", "value", {path: "/"});
+        return tongs().remove("name", {path: "/"});
       });
 
       expect(result).toEqual(true);
 
-      browser.manage().getCookie('name').then(function(cookie) {
+      browser.manage().getCookie("name").then(function(cookie) {
         expect(cookie).toBeNull();
       });
     });
